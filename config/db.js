7509d7e4724076bb974a7631f32b74d7aa3290ac
Jimmy Ventura <jimmy.ventura@tecsup.edu.pe>
2025-07-02 14:26:52 -0500

    Arreglos con conexion de bse de dtos

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS, // <-- aquí debe ser DB_PASS
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
