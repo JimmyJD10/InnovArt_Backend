cf1186438e84809b21d3872867824c22ccbb20fa
Jimmy Ventura <jimmy.ventura@tecsup.edu.pe>
2025-07-07 00:37:44 -0500

    Actualizacion JWT

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/pedidos', pedidoRoutes);thMiddleware');
+app.use('/api/pedidos', pedidoRoutes);
 
 // Ejemplo: proteger pedidos
 app.use('/api/pedidos', auth, pedidoRoutes);tes');
